bb1943e591d31195e489c0f44a1811a7fcc7bad2
hiracy <leizhen@mbr.nifty.com>
2017-01-18 21:07:48 +0900

    Rename api package

---
+++ api/api.go
@@ -0,0 +1,10 @@
+package api
+
+// API is the object of basic login for zabbbix server
+type API struct {
+	Host       string
+	Path       string
+	User       string
+	Password   string
+	APIVersion string
+}
