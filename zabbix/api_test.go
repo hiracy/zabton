599b8080ae8820f322ace06dd74303f42d6a2f63
hiracy <leizhen@mbr.nifty.com>
2017-09-22 17:19:56 +0900

    Add test

--- zabbix/api_test.go
+++ zabbix/api_test.go
@@ -37,3 +37,47 @@ func TestLogin(t *testing.T) {
 
 	t.Logf("authentication token: %s", auth)
 }
+
+func TestGetHost(t *testing.T) {
+	testAPI := NewAPI(
+		testZabbixURL,
+		testZabbixUser,
+		testZabbixPassword)
+	testAPI.Auth = testAuth
+
+	params := make(map[string]interface{})
+	params["output"] = "extend"
+	params["selectGroups"] = "extend"
+	params["selectParentTemplates"] = "extend"
+
+	hosts, err := testAPI.GetHost(params)
+
+	if err != nil {
+		t.Fatalf("GetHost() failed: %s", err)
+	}
+
+	found := false
+	for _, obj := range hosts {
+		o, ok := obj.(map[string]interface{})
+		if ok {
+			if v, ok := o["name"]; ok {
+				found = true
+				if v != "Zabbix server" {
+					t.Errorf("GetHost() failed: %s", v)
+				}
+			}
+			if v, ok := o["hostid"]; ok {
+				found = true
+				if v != "10084" {
+					t.Errorf("GetHost() failed: %s", v)
+				}
+			}
+		} else {
+			t.Errorf("Irregular format: %T", obj)
+		}
+	}
+
+	if !found {
+		t.Error("GetHost() failed: empty")
+	}
+}
